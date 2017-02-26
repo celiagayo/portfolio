8c5a78b8956384029fd79c06cc72c3e2ad24b25e
Celia Gayo <celiagayo.e@gmail.com>
2017-02-26 20:16:09 +0100

    estructura principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,36 @@
 @extends('layout.master')
-@section('title', 'Home')
-
+@section('title', 'Celia Gayo Escribano')
 @section('content')
 
 
-<div class="showmenu">
-    @include('layout.navbar')
-</div>
+<div class="portada">
+    <h1 class="titulo">Celia Gayo Escribano</h1>
+    <h2 class="titulo"><em>diseñadora</em></h2>
 
+    <ul class="titulo">
+        <li class="c-web">Web</li>
+        <li class="c-graphic">Graphic</li>
+        <li class="c-producto">Producto</li>
+        <li class="c-arquitectura">Arquitectura</li>
+    </ul>
+</div>
 <div class="images">
     @foreach ($categories as $category)
-        <div>{{ $category->id }}</div>
-        <div>{{ $category->slug }}</div>
-        <div>{{ $category->title }}</div>
-        <div>{{ $category->detail }}</div>
+    <div>{{ $category->id }}</div>
+    <div>{{ $category->slug }}</div>
+    <div>{{ $category->title }}</div>
+    <div>{{ $category->detail }}</div>
 
-        @foreach ($category->projects as $project)
-            <div>{{ $project->id }}</div>
-            <div>{{ $project->slug }}</div>
-            <div>{{ $project->title }}</div>
-            <div>{{ $project->url }}</div>
-            <div>{{ $project->featured_image }}</div>
-            <div>{{ $project->detail_one }}</div>
-            <div>{{ $project->detail_two }}</div>
-            
-        @endforeach
+    @foreach ($category->projects as $project)
+    <div>{{ $project->id }}</div>
+    <div>{{ $project->slug }}</div>
+    <div>{{ $project->title }}</div>
+    <div>{{ $project->url }}</div>
+    <div>{{ $project->featured_image }}</div>
+    <div>{{ $project->detail_one }}</div>
+    <div>{{ $project->detail_two }}</div>
+
+    @endforeach
     @endforeach
 </div>
 @endsection
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,23 +1,24 @@
+<!DOCTYPE html>
 <html>
     <head>
         <title> @yield('title') </title>
-        <!--        <link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet"> -->
-        <link href="https://fonts.googleapis.com/css?family=Archivo+Narrow|Open+Sans+Condensed:300|Quicksand:300|Questrial" rel="stylesheet">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Portfolio personal Celia Gayo Escribano">
+      <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:400,400i,500,500i,600,600i" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Raleway:300i" rel="stylesheet">
         <!-- CSS files -->
         <link rel="stylesheet" href="/css/app.css">
-       
+
 
         <!-- Javascript files -->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
- 
         <script src="/js/app.js"></script>
 
     </head>
     <body >
         <header>
-            <div class="cabecera-fija showmenu">
-            <!--@include('layout.navbar')-->
-            </div>
+<!--            @include('layout.navbar')-->
         </header>
 
         @yield('content')
