4043c70c63170efe9c3f578b675e2976c1dfa7dd
Alvaro Reig Gonzalez <alvaroreig@gmail.com>
2017-02-26 11:20:17 +0100

    metiendo alias de sass

--- misc/alias.sh
+++ misc/alias.sh
@@ -1,3 +1,11 @@
+# Instalar dependencias
+composer install
+npm install
+
+# Recrear base de datos
 docker exec portfolio-mysql mysql -u portfolio-user -plocaldatabase portfolio -e "drop database portfolio;CREATE DATABASE portfolio CHARACTER SET utf8 COLLATE utf8_general_ci;"
 docker exec portfolio-php-fpm php artisan migrate
 docker exec portfolio-php-fpm php artisan db:seed
+
+# Demonio que compila de sass a css, entre otros.
+docker exec portfolio-php-fpm npm run watch
