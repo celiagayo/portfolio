10f89cace00ab336e88b8e11da87b726e8e9ec9f
Alvaro Reig Gonzalez <alvaroreig@gmail.com>
2017-08-14 19:08:59 +0200

    eliminando codigo ñapa no necesario

--- app/Category.php
+++ app/Category.php
@@ -15,13 +15,4 @@ class Category extends Model
     {
         return $this->belongsToMany('App\Project');
     }
-
-    public function getProject($project_slug){
-    	return DB::table('projects')
-    		-> join('category_project','projects.id','category_project.project_id')
-    		->where('category_project.category_id', $this->id)
-    		->where('projects.slug','=',$project_slug)
-    		->select('projects.*')
-    		->first();
-    }
 }
